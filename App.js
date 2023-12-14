8253bda3480687f6cedeec860c90119063a632d9
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-14 23:52:13 +0530

    started the project

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './App/Pages/Login';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>heelloo jii</Text>
-      <StatusBar style="auto" />
+    <View >
+      <Login/>
     </View>
   );
 }
