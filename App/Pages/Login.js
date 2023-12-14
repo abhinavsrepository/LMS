b5170e5029435d02e18ac9d34a6a930f72d453b5
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-15 00:41:21 +0530

    loginpage

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,12 +1,26 @@
-import { View, Text ,Image} from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
 
 export default function Login() {
   return (
     <View>
-      
-      <Image source ={require('./../Assets/Images/login.jpg')}/>
-    <Text style={{fontSize:35,textAlign:'center',fontWeight:'bold'}}>Welcome to Abhinav's Academy</Text>
+      <Image source={require("./../Assets/Images/login.jpg")} />
+      <View style={styles.container}>
+      <Text style={styles.welcomeText}>Welcome to Abhinav's Academy</Text>
+
+      </View>
+     
     </View>
-  )
+  );
 }
+const styles = StyleSheet.create({
+    container:{
+        paddingTop:40
+
+    },
+    welcomeText:{fontSize: 35,
+    textAlign: "center",
+    fontWeight: "bold",
+    }
+    
+});
