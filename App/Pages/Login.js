2730a9c8eac0779892030fb1d0fdd5853e5d7ddf
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-14 23:55:29 +0530

    image added

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text ,Image} from 'react-native'
 import React from 'react'
 
 export default function Login() {
   return (
     <View>
-      <Text>Login</Text>
+      <Image source ={require('./../Assets')}/>
     </View>
   )
 }
