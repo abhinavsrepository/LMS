226efd354a7b6a4802f96ef361bd6085a2bf3d0e
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-12-18 23:23:18 +0530

    login done  spcl

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, Colors } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 
